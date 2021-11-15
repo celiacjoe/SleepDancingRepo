0c067bea8585d7701f51346be8abdb00bd364e90
Celia_Betourne <Celia.betourne@gmail.com>
2021-11-15 17:43:04 +0100

    init projet

---
+++ HDRP_Vfx/Assets/CamControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public Animator AC_Cam;
+    public Camera Cam;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("h"))
+        {
+            AC_Cam.SetTrigger("RotationHorizontal");
+        }
+        if (Input.GetKeyDown("v"))
+        {
+            Debug.Log("verticalok");
+            AC_Cam.SetTrigger("RotationVertical");
+        }
+        if (Input.GetKeyDown("t"))
+        {
+            AC_Cam.SetTrigger("Througt");
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            Cam.orthographic = true;
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            Cam.orthographic = false;
+        }
+    }
+}
